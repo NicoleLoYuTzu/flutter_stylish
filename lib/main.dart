20a2bdf6d715ed6dd8a5e1641aa8168cf68ee066
LouNicole-YM <ymrd.nicole@gmail.com>
2023-03-29 15:59:00 +0800

    抽widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,52 +11,52 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+int _selectedIndex = -1;
+final List<String> _categories = ['女裝', '男裝', '配件'];
+final List<List<String>> _items = [
+  [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10'
+  ],
+  [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+    'Item 11',
+    'Item 12'
+  ],
+  [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10'
+  ],
+];
+
+final List<bool> _itemsExpansionPanel = [false, false, false];
+
 class _MyAppState extends State<MyApp> {
-  int _selectedIndex = -1;
-  final List<String> _categories = ['女裝', '男裝', '配件'];
-  final List<List<String>> _items = [
-    [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-      'Item 5',
-      'Item 6',
-      'Item 7',
-      'Item 8',
-      'Item 9',
-      'Item 10'
-    ],
-    [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-      'Item 5',
-      'Item 6',
-      'Item 7',
-      'Item 8',
-      'Item 9',
-      'Item 10',
-      'Item 11',
-      'Item 12'
-    ],
-    [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-      'Item 5',
-      'Item 6',
-      'Item 7',
-      'Item 8',
-      'Item 9',
-      'Item 10'
-    ],
-  ];
-
-  final List<bool> _itemsExpansionPanel = [false, false, false];
-
   @override
   Widget build(BuildContext context) {
     final double appBarHeight = AppBar().preferredSize.height;
@@ -65,202 +65,108 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         body: Column(
           children: [
-            SizedBox(
-              height: 360.0,
-              child: PageView.builder(
-                itemCount: 3,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset(
-                        'images/${index + 1}.jpeg',
-                        fit: BoxFit.cover,
-                        height: 100.0,
-                      ),
-                    ),
-                  );
-                },
-                pageSnapping: true,
-                scrollDirection: Axis.horizontal,
-                controller:
-                PageController(initialPage: 1, viewportFraction: 0.3),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                for (int i = 0; i < _categories.length; i++)
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _selectedIndex = i;
-                        _itemsExpansionPanel.fillRange(
-                            0, _itemsExpansionPanel.length, false);
-                        _itemsExpansionPanel[0] = i == 0;
-                      });
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: i == _selectedIndex
-                                ? Colors.black
-                                : Colors.transparent,
-                            width: 2.0,
-                          ),
-                        ),
-                      ),
-                      child: Text(
-                        _categories[i],
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                          color:
-                          i == _selectedIndex ? Colors.black : Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-            if (_selectedIndex >= 0)
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  child: SingleChildScrollView(
-                    child: ExpansionPanelList(
-                        expansionCallback: (int index, bool isExpanded) {
-                          setState(() {
-                            _itemsExpansionPanel[index] = !isExpanded;
-                          });
-                        },
-                        children: [
-                          ExpansionPanel(
-                            isExpanded: _itemsExpansionPanel[0],
-                            headerBuilder:
-                                (BuildContext context, bool isExpanded) {
-                              return Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Text(
-                                  _categories[_selectedIndex],
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              );
-                            },
-                            body: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0),
-                              child: Column(
-                                children: [
-                                  for (int i = 0; i < _items[_selectedIndex].length; i++)
-                                    ListTile(
-                                      title: Text(_items[_selectedIndex][i]),
-                                    )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ]),
-                  ),
-                ),
-              )
+            const PictureOnTop(),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return (constraints.maxWidth < 600)
+                    ? _buildVerticalCategories() // 如果螢幕寬度小於 600，則使用垂直列表
+                    : _buildHorizontalCategories(); // 否則使用水平列表
+              },
+            )
           ],
         ),
       ),
     );
-  }}
+  }
 
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _selectCategory(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
+      _itemsExpansionPanel.fillRange(0, _itemsExpansionPanel.length, false);
+      _itemsExpansionPanel[0] = index == 0;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headlineMedium,
-            ),
-          ],
+  Widget _buildVerticalCategories() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: List.generate(
+        _categories.length,
+        (i) => GestureDetector(
+          onTap: () => _selectCategory(i),
+          child: _buildCategoryItem(i),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildHorizontalCategories() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: List.generate(
+        _categories.length,
+        (i) => GestureDetector(
+          onTap: () => _selectCategory(i),
+          child: _buildCategoryItem(i),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryItem(int index) {
+    final bool isSelected = index == _selectedIndex;
+
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: isSelected ? Colors.black : Colors.transparent,
+            width: 2.0,
+          ),
+        ),
+      ),
+      child: Text(
+        _categories[index],
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+          color: isSelected ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
+  }
+}
+
+class PictureOnTop extends StatelessWidget {
+  const PictureOnTop({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 360.0,
+      child: PageView.builder(
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(
+                'images/${index + 1}.jpeg',
+                fit: BoxFit.cover,
+                height: 100.0,
+              ),
+            ),
+          );
+        },
+        pageSnapping: true,
+        scrollDirection: Axis.horizontal,
+        controller: PageController(initialPage: 1, viewportFraction: 0.3),
+      ),
     );
   }
 }
