aff3596bc941d56ab8399d3c6b5d1ec8fc20a798
LouNicole-YM <ymrd.nicole@gmail.com>
2023-04-24 17:38:04 +0800

    [add]加入android arr, 錯誤

---
+++ android/app/src/main/kotlin/com/example/flutter_stylish/Constants.java
@@ -0,0 +1,15 @@
+package tech.cherri.directpayexample;
+
+
+public class Constants {
+
+    public static final TPDServerType SERVER_TYPE = TPDServerType.Sandbox;
+    public static final String TAPPAY_DOMAIN = "https://sandbox.tappaysdk.com/tpc";
+    public static final String TAPPAY_PAY_BY_PRIME_URL = "/payment/pay-by-prime";
+
+    public static final String PARTNER_KEY = "your partner key";
+    public static final String APP_KEY = "your app key";
+    public static final Integer APP_ID = 0;
+    public static final String MERCHANT_ID = "your merchant id";
+
+}
